ecca762729803bcf842a82f4d56622aa5394fc43
Cyebukayire <peacecyebukayire@gmail.com>
2021-06-02 15:05:16 +0200

    Hibernate

---
+++ src/main/java/edu/school/man/dao/StudentDaoHbnt.java
@@ -0,0 +1,126 @@
+package com.example.JspStudentCrud.DB;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import com.example.JspStudentCrud.models.Student;
+import com.example.JspStudentCrud.utils.HibernateUtil;
+
+public class StudentDaoHbnt {
+
+    /**
+     * Save Student
+     *
+     * @param student
+     */
+    public void saveStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.save(student);
+            // commit transaction
+            transaction.commit();
+            System.out.println(" New student added using hibernate okey");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Update Student
+     *
+     * @param student
+     */
+    public void updateStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.update(student);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Delete Student
+     *
+     * @param id
+     */
+    public void deleteStudent(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // Delete a student object
+            Student student = session.get(Student.class, id);
+            if (student != null) {
+                session.delete(student);
+                System.out.println("student is deleted");
+            }
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Get Student By ID
+     *
+     * @param id
+     * @return
+     */
+    public Student getStudent(int id) {
+        Transaction transaction = null;
+        Student student = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // get an student object
+            student = session.get(Student.class, id);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return student;
+    }
+    /**
+     * Get all Students
+     *
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public List<Student> getAllStudent() {
+        Transaction transaction = null;
+        List<Student> listOfStudent = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // get an student object
+            listOfStudent = session.createQuery("from Student").getResultList();
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return listOfStudent;
+    }
+}
